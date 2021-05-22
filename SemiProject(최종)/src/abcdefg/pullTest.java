1da9fa57ea43a041258d96c7e8e53dc4dc33b6c3
SeoJunsik <tjwnstlr3459@naver.com>
2021-05-23 00:16:24 +0900

    pull오류 확인

---
+++ SemiProject(최종)/src/abcdefg/pullTest.java
@@ -0,0 +1,26 @@
+package abcdefg;
+
+public class pullTest {
+
+	private String junsik;
+
+	public pullTest() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public pullTest(String junsik) {
+		super();
+		this.junsik = junsik;
+	}
+
+	public String getJunsik() {
+		return junsik;
+	}
+
+	public void setJunsik(String junsik) {
+		this.junsik = junsik;
+	}
+	
+	
+}
